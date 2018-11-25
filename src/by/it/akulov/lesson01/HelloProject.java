55c1399fed8ee4cdeb7cdcaf9c06c77bbf9a58c4
Anastasiya Shchura <anastasiya.shchura@gmail.com>
2018-11-25 18:21:00 +0300

    other people packages were removed

--- src/by/it/akulov/lesson01/HelloProject.java
+++
@@ -1,7 +0,0 @@
-package by.it.akulov.lesson01;
-
-public class HelloProject {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!\n\nGreat regards, Dmitry Akulov!");
-    }
-}
